108e7abb47bb7ecd96b99f86fb8366e942123371
Ahemd Hussein Morgan <136130399+ahmed-hussein-morgan@users.noreply.github.com>
2024-03-11 12:46:19 +0000

    back to old version

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,15 @@
-#!/usr/bin/python3
 import uuid
 import datetime
-import models
 
 
 class BaseModel:
-    """the base of all models"""
+    """base is important"""
     def __init__(self, *args, **kwargs):
-        """initialization"""
+        """hey it is ini"""
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
-        models.storage.new(self)
 
         if kwargs is not None:
             for key, value in kwargs.items():
@@ -22,18 +20,17 @@ class BaseModel:
 
                 setattr(self, key, value)
 
+
     def __str__(self) -> str:
-        """the string represintation"""
+        """come to pri"""
         return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """save the obj"""
-        models.storage.save()
+        """rmmber me"""
         self.updated_at = datetime.datetime.now()
-        #models.storage.save()
 
     def to_dict(self):
-        """convert to dictionary"""
+        """open the book"""
         self.created_at = str(datetime.datetime.isoformat(self.created_at))
         self.updated_at = str(datetime.datetime.isoformat(self.updated_at))
         self.__dict__['__class__'] = __class__.__name__
