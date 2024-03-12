56478c99f39e62ea8a096d619e4474e557eb0610
Ahemd Hussein Morgan <136130399+ahmed-hussein-morgan@users.noreply.github.com>
2024-03-12 10:30:24 +0000

    try to re test

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -7,13 +7,25 @@ class TestPlaceModel(unittest.TestCase):
     """test Review class"""
 
     my_place = Place()
-    def test_attributes(self):
+    def test_attribute1(self):
         self.assertIsInstance(self.my_place.name, str)
+
+    def test_attribute2(self):
         self.assertIsInstance(self.my_place.user_id, str)
+    
+    def test_attribute3(self):
         self.assertIsInstance(self.my_place.city_id, str)
+
+    def test_attribute4(self):
         self.assertIsInstance(self.my_place.description, str)
+    
+    def test_attribute5(self):
         self.assertIsInstance(self.my_place.number_rooms, int)
+
+    def test_attribute6(self):
         self.assertIsInstance(self.my_place.number_bathrooms, int)
+
+    def test_attribute7(self):
         self.assertIsInstance(self.my_place.max_guest, int)
         self.assertIsInstance(self.my_place.price_by_night, int)
         self.assertIsInstance(self.my_place.latitude, float)
