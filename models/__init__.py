399b9d600193c9286216ccf4c2c1ba0108c5538d
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-05-14 11:19:21 +0300

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
-"""the link between models and engine"""
-from models.engine.file_storage import FileStorage
+# #!/usr/bin/python3
+# """the link between models and engine"""
+# from models.engine.file_storage import FileStorage
 
 
-storage = FileStorage()
-storage.reload()
+# storage = FileStorage()
+# storage.reload()
