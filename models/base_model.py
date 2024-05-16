e505c5875a1b68e42f49ac0d11608fa8a9f07b1d
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-05-16 13:05:21 +0300

    update - base model - to dict

--- models/base_model.py
+++ models/base_model.py
@@ -67,7 +67,7 @@ class BaseModel:
         # dictionary['created_at'] = self.created_at.isoformat()
         # dictionary['updated_at'] = self.updated_at.isoformat()
         # return dictionary
-        self.created_at = datetime.datetime.isoformat(self.created_at)
-        self.updated_at = datetime.datetime.isoformat(self.updated_at)
+        self.created_at = datetime.isoformat(self.created_at)
+        self.updated_at = datetime.isoformat(self.updated_at)
         self.__dict__.__setitem__('__class__', self.__class__.__name__)
         return self.__dict__
